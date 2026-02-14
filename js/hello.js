cd27aa5f7e8abaf8f0cc42a87340e26b18b59977
mdallamaiqbal <bd999plus13@gmail.com>
2026-02-14 21:03:49 +0600

    add style css usig dom

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,13 @@
 console.log('Hello outSide');
+const sections=document.querySelectorAll('section');
+for(const section of sections){
+    console.log(section);
+    section.style.backgroundColor='aqua';
+    section.style.borderRadius='20px'
+    section.style.padding='20px'
+    section.style.margin='10px'
+}
+const ulLi=document.querySelectorAll('#addStyle');
+for(const ul of ulLi){
+    ul.classList.add('dom')    
+}
